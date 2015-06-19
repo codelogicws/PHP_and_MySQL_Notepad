26f9992af90ca38d17cc03dda3153a276dde3c0d
codelogicws <russ@codelogic.ws>
2015-06-18 20:03:02 -0700

    commit 2

--- notelist.php
+++ notelist.php
@@ -7,12 +7,27 @@ include "MySQLConnection.php";
 
 <?php
 
-    echo "<br>";
-    $conn = new MySQLConnection($_POST["password"]);
-    $titles = $conn->select();
-    foreach($titles as $key => $val){
-        echo $val . " (" . $key . ")<br>";
-    }
+
+function addButton($key, $title){
+    echo $title . " (" . $key . ")<br>\n";
+    echo '<button type="submit" name="note" class="bttn" value="' .
+        $key . 
+        '">' . 
+        $title . 
+        "</button><br>";
+}
+
+echo "<br>";
+$conn = new MySQLConnection($_POST["password"]);
+$titles = $conn->select();
+
+echo '<form action="editnote.php" method="post">';
+
+foreach($titles as $key => $title){
+    addButton($key, $title);
+}
+
+echo "</form>";
 
 ?>
 
