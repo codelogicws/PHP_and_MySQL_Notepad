3115e5070db863d30bc65517719e58842db84c08
codelogicws <russ@codelogic.ws>
2015-06-21 00:56:49 -0700

    The User can now make new notes from the UI.

--- createdNote.php
+++ createdNote.php
@@ -1,2 +1,14 @@
 <?php
-echo "Creating Note";
+
+include "MySQLConnection.php";
+
+$title = $_POST["title"];
+$note = $_POST["note"];
+
+
+echo $title;
+echo "<br>";
+echo $note;
+
+$connection = MySQLConnection::makeWithPassword($_SESSION["password"]);
+echo $connection->addNote($title, $note);
